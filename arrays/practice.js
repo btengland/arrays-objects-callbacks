25eff5475e51624c769934d78e07cddac0513aad
Brady England <bradyeng22@gmail.com>
2021-01-07 15:35:37 -0700

    Finished part one

--- arrays/practice.js
+++ arrays/practice.js
@@ -17,8 +17,9 @@ var arr = [10,20,30];
   Return the first item in the given array.
 */
 
-//Code Here
-
+function first(arr){
+  return arr[0]
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -32,7 +33,9 @@ var arr = [40,50,60];
   Return the last item in the given array.
 */
 
-//Code Here
+function last(arr){
+  return arr [2]
+}
 
 
 
@@ -47,9 +50,12 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
   Loop through the given array and alert every item in the array.
 */
 
-//Code Here
+function looper(family){
+  for(let i = 0; i < family.length; i++){
+  alert(family[i])}
+}
 
-
+looper(family)
 
 ////////// PROBLEM 4 //////////
 
@@ -62,8 +68,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop backwards, starting at the end of the letters array, alerting every item in the array.
 */
 
-//Code Here
-
+function reversedLooper(letters){
+  for(let i = letters.length - 1; i >= 0; i--){
+    alert(letters[i])
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -77,8 +86,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
   Return an array that contains the even numbers from the nums array.
 */
 
-//Code Here
-
+function evenFinder(nums){
+  let evenArray = []
+  for(let i = 0; i < nums.length; i++){
+    if(nums[i] % 2 === 0){
+      evenArray.push(nums[i])
+    }
+  }
+  return evenArray
+}
 
 
 
